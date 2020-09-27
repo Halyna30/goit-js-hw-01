0e5d4a6badfac51ad65f1abeaf4e6fa2fd9cba59
Halyna Pryn <prynhalyna@gmail.com>
2020-09-27 19:20:44 +0200

    Розв'язані завдання 1,2 модуля

---
+++ js/task-номер_6.js
@@ -0,0 +1,27 @@
+const colors = ['red', 'blue', 'brown', 'yellow', 'black'];
+console.table(colors);
+
+// Методи join i split
+let newColors = colors.join();
+
+// console.log(newColors);
+
+//console.log(newColors.split(','));
+
+// Методи indexof i includes
+// const index = colors.indexOf('brown');
+
+// console.log(index);
+
+// console.log(colors.includes('red'));
+
+// const oneColor = colors.pop();
+// console.log(oneColor);
+
+// const newColection = colors.push();
+// console.log(newColection);
+
+// const firstElement = colors.shift();
+// console.log(firstElement);
+
+console.log(colors.unshift('white'));
